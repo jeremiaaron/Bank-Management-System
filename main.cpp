e606c0e0711cca0debb5132700090054f0977d14
Brandon John <80831628+BoomCheeseBurge@users.noreply.github.com>
2021-12-06 15:43:20 +0700

    Updates on Menu and Tweaks
    Added the modify, delete, and find a customer menu for admin. Added the check account info, deposit, and withdraw menu for customer. Changed name of "log history" to "transaction history". Queue for transaction history both admin and customer will follow up.

--- main.cpp
+++ main.cpp
@@ -36,7 +36,7 @@ void adminMenu() {
     cout << "3. Modify a customer account" << endl;
     cout << "4. Delete a customer account" << endl;
     cout << "5. Find a customer account" << endl;
-    cout << "6. Display log history of selected customer" << endl;
+    cout << "6. Display transaction history of selected customer" << endl;
     cout << "0. Go back to main menu" << endl;
     
     cout << "Enter your option: ";
@@ -44,15 +44,21 @@ void adminMenu() {
     getchar();
 
     if(menu == 1) {
+    	
         system("cls");
+        
         accounts->displayAllAccounts(accounts);
 
-        cout << "1. Go back to Administrators Menu" << endl;
-        cout << "2. Go back to Main Menu" << endl;
+		cout << "========================================" << endl;;
+        cout << "|	1. Go back to Administrator's Menu	|" << endl;
+        cout << "|	2. Go back to Main Menu          	|" << endl;
+        cout << "========================================" << endl;;
         
         int option;
+        
         cout << "Enter your option: ";
         cin >> option;
+        
         if(option == 1)
             adminMenu();
         if(option == 2)
@@ -65,14 +71,17 @@ void adminMenu() {
         string gender;
         int num, accNum;
         int pin;
-        int bal;
+        float bal;
         char option;
 
         do {
             system("cls");
 
-            cout << "Enter customer's full name: ";
+            cout << "Enter customer's full name (Enter "back" to menu): ";
             getline(cin, name);
+            
+            if(name == "back")
+            	adminMenu();
 
             cout << "Enter customer's NIK: ";
             cin >> nik; getchar();
@@ -99,26 +108,218 @@ void adminMenu() {
             cin >> option; getchar();
 
             if(option == 'N' || option == 'n')
-                adminMenu();
+            {
+            	system("cls");
+            	
+            	cout << "========================================" << endl;;
+        		cout << "|	1. Go back to Administrator's Menu	|" << endl;
+        		cout << "|	2. Go back to Main Menu          	|" << endl;
+        		cout << "========================================" << endl;;
+        
+        		int option;
+        		
+        		cout << "Enter your option: ";
+        		cin >> option;
+        		
+        		if(option == 1)
+            		adminMenu();
+        		if(option == 2)
+            		main();
+			}
 
             cout << endl;
         } while(option == 'Y' || option == 'y');
     }
 
     else if(menu == 3) {
+		string name;
+        string nik;
+        string gender;
+        int num;
+        int pin;
+        float bal;
+        char option;
+        
+        do {
+            system("cls");
 
+			do
+			{
+				cout << "Enter the Account's No. to be modified (1 to menu): ";
+				cin >> num;
+				
+				if(num == 1)
+					adminMenu();
+				
+				if(accounts->findAccount(accounts, num))
+					continue;
+				else
+					num = 0;
+					cout << "Account does not exist" << endl;
+			} while(num < 12410000 || num > 12419999);
+			
+			system("cls");
+
+			cout << "==================================" << endl;;
+			cout << "|\tEnter New Account Info Below\t|" << endl;;
+			cout << "==================================" << endl;;
+			
+            cout << "Enter new Account's name: ";
+            getline(cin, name);
+
+            cout << "Enter new Account's NIK: ";
+            cin >> nik; getchar();
+
+            cout << "Enter new Account's gender (F/M): ";
+            cin >> gender; getchar();
+
+            cout << "The customer's account number is " << num << endl;
+
+            cout << "Enter customer's account PIN: ";
+            cin >> pin; getchar();
+            
+            cout << "Enter the amount of initial deposit: ";
+            cin >> bal; getchar();
+            
+            accounts->modifyInfo(accounts, pin, num, name, nik, gender, bal);
+
+            cout << "Do you want to modify another account? (Y/N): ";
+            cin >> option; getchar();
+
+            if(option == 'N' || option == 'n')
+            {
+            	system("cls");
+            	
+            	cout << "========================================" << endl;;
+        		cout << "|	1. Go back to Administrator's Menu	|" << endl;
+        		cout << "|	2. Go back to Main Menu          	|" << endl;
+        		cout << "========================================" << endl;;
+        
+        		int option;
+        		
+        		cout << "Enter your option: ";
+        		cin >> option;
+        		
+        		if(option == 1)
+            		adminMenu();
+        		if(option == 2)
+            		main();
+			}
+			
+            cout << endl;
+        } while(option == 'Y' || option == 'y');
     }
 
     else if(menu == 4) {
+    	
+    	system("cls");
+    	
+    	int num;
+    	char option;
+    	
+		do
+		{
+			do
+			{
+				cout << "Enter the Account's No. to be deleted (1 to menu): ";
+				cin >> num;
+				
+				if(num == 1)
+					adminMenu();
+				
+				if(accounts->findAccount(accounts, num))
+					continue;
+				else
+					num = 0;
+					cout << "Account does not exist" << endl;
+			} while(num < 12410000 || num > 12419999);
+			
+			accounts = accounts->deleteAccount(accounts, num);
+			
+			cout << "Account has been successfully delete!" << endl;
+			
+			cout << "Do you want to delete another account? (Y/N): ";
+            cin >> option; getchar();
 
+            if(option == 'N' || option == 'n')
+            {
+            	system("cls");
+            	
+            	cout << "========================================" << endl;;
+        		cout << "|	1. Go back to Administrator's Menu	|" << endl;
+        		cout << "|	2. Go back to Main Menu          	|" << endl;
+        		cout << "========================================" << endl;;
+        
+        		int option;
+        		
+        		cout << "Enter your option: ";
+        		cin >> option;
+        		
+        		if(option == 1)
+            		adminMenu();
+        		if(option == 2)
+            		main();
+			}
+
+            cout << endl;
+		} while(option == 'Y' || option == 'y');
     }
 
     else if(menu == 5) {
+    	
+    	system("cls");
+    	
+    	int num;
+    	char option;
+    	
+		do
+		{
+			do
+			{
+				cout << "Enter the Account's No. to be displayed (1 to menu): ";
+				cin >> num;
+				
+				if(num == 1)
+					adminMenu();
+				
+				if(findAccount(accounts, num))
+					continue;
+				else
+					num = 0;
+					cout << "Account does not exist" << endl;
+			} while(num < 12410000 || num > 12419999);
+			
+			accounts->showAccountInfo(accounts, num);
+			
+			cout << "Do you want to view another account? (Y/N): ";
+            cin >> option; getchar();
 
+            if(option == 'N' || option == 'n')
+            {
+            	system("cls");
+            	
+            	cout << "========================================" << endl;;
+        		cout << "|	1. Go back to Administrator's Menu	|" << endl;
+        		cout << "|	2. Go back to Main Menu          	|" << endl;
+        		cout << "========================================" << endl;;
+        
+        		int option;
+        		
+        		cout << "Enter your option: ";
+        		cin >> option;
+        		
+        		if(option == 1)
+            		adminMenu();
+        		if(option == 2)
+            		main();
+			}
+
+            cout << endl;
+		} while(option == 'Y' || option == 'y');*/
     }
 
     else if(menu == 6) {
-
+		
     }
 
     else if(menu == 0)
@@ -129,6 +330,7 @@ void adminMenu() {
 }
 
 int customerMenu() {
+	
     system("cls");
 
     int menu;
@@ -136,9 +338,9 @@ int customerMenu() {
     cout << "\tCUSTOMERS MENU\t\t" << endl;
     cout << "======================================" << endl << endl;
     cout << "1. Check your account info" << endl;
-    cout << "2. Delete a customer account" << endl;
-    cout << "3. Find a customer account" << endl;
-    cout << "4. Display log history of selected customer" << endl;
+    cout << "2. Deposit funds" << endl;
+    cout << "3. Withdraw funds" << endl;
+    cout << "4. Display transaction history of selected customer" << endl;
     cout << "0. Go back to main menu" << endl;
     
     cout << "Enter your option: ";
@@ -146,6 +348,165 @@ int customerMenu() {
     getchar();
 
     system("cls");
+    
+    if(menu == 1) {
+    	
+    	system("cls");
+    	
+		int num;
+    	
+    	cout << "Enter your Bank Account No. (1 to menu): ";
+    	cin >> num;
+    	
+    	if(num == 1)
+    		customerMenu();
+    	
+    	system("cls");
+        
+        accounts->showAccountInfo(accounts, num);
+
+		cout << "========================================" << endl;;
+        cout << "|	1. Go back to Customer's' Menu		|" << endl;
+        cout << "|	2. Go back to Main Menu          	|" << endl;
+        cout << "========================================" << endl;;
+        
+        int option;
+        
+        cout << "Enter your option: ";
+        cin >> option;
+        
+        if(option == 1)
+            customerMenu();
+        if(option == 2)
+            main();
+    }
+
+    else if(menu == 2) {
+    	
+    	system("cls");
+    	
+    	int pin;
+    	int num;
+    	float dep;
+    	
+		do
+		{
+			do
+			{
+				cout << "Enter your Bank Account No. (1 to menu): ";
+				cin >> num;
+				
+				if(num == 1)
+					customerMenu();
+				
+				cout << "Enter your PIN : ";
+				cin >> pin;
+				
+				system("cls");
+
+			} while(!accounts->pinValidator(accounts, num, pin));
+			
+			
+			cout << "Enter amount of deposit: ";
+			cin << dep;
+			
+			accounts->depositFunds(accounts, num, dep);
+			
+			cout << "Deposit Success" << endl;
+			
+			cout << "Do you want to deposit again? (Y/N): ";
+            cin >> option; getchar();
+
+            if(option == 'N' || option == 'n')
+            {
+            	system("cls");
+            	
+            	cout << "========================================" << endl;;
+        		cout << "|	1. Go back to Customer's Menu		|" << endl;
+        		cout << "|	2. Go back to Main Menu          	|" << endl;
+        		cout << "========================================" << endl;;
+        
+        		int option;
+        		
+        		cout << "Enter your option: ";
+        		cin >> option;
+        		
+        		if(option == 1)
+            		adminMenu();
+        		if(option == 2)
+            		main();
+			}
+
+            cout << endl;
+		} while(option == 'Y' || option == 'y');
+    }
+
+    else if(menu == 3) {
+    	
+    	system("cls");
+    	
+    	int pin;
+    	int num;
+    	float wit;
+    	
+		do
+		{
+			do
+			{
+				cout << "Enter your Bank Account No. (1 to menu): ";
+				cin >> num;
+				
+				if(num == 1)
+					customerMenu();
+				
+				cout << "Enter your PIN : ";
+				cin >> pin;
+				
+				system("cls");
+
+			} while(!accounts->pinValidator(accounts, num, pin));
+			
+			do
+			{
+				cout << "Enter amount to withdraw: ";
+				cin << wit;
+				
+				system("cls");
+				
+			}(!accounts->withdrawFunds(accounts, num, wit);)
+			
+			cout << "Withdraw Success" << endl;
+			
+			cout << "Do you want to deposit again? (Y/N): ";
+            cin >> option; getchar();
+
+            if(option == 'N' || option == 'n')
+            {
+            	system("cls");
+            	
+            	cout << "========================================" << endl;;
+        		cout << "|	1. Go back to Customer's Menu		|" << endl;
+        		cout << "|	2. Go back to Main Menu          	|" << endl;
+        		cout << "========================================" << endl;;
+        
+        		int option;
+        		
+        		cout << "Enter your option: ";
+        		cin >> option;
+        		
+        		if(option == 1)
+            		adminMenu();
+        		if(option == 2)
+            		main();
+			}
+
+            cout << endl;
+		} while(option == 'Y' || option == 'y');
+    }
+
+    else if(menu == 4) {
+		
+    }
 
     return menu;
 }
